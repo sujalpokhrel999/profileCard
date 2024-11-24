3727b56f8fe82a3cc4ce0ea71c56a34044220d2d
sujalpokhrel999 <sujal3pokhrel@gmail.com>
2024-11-24 13:44:02 +0545

    Update React components and styles

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,16 +8,16 @@ function Footer(){
             <p className="CardText">Hi, I'm Sujal Pokhrel, a dedicated Front-End Developer with expertise in HTML, CSS, JavaScript, and React.js. I thrive on creating engaging, responsive, and dynamic web applications that offer seamless user experiences. Let's build something amazing together!</p>
            </div>
            <div className="flex gap-4 icons">
-            <a href="#" className="text-blue-400 hover:text-blue-300 transition-colors facebook">
+            <a href="https://www.facebook.com/sujal.pokhrel.75" className="text-blue-400 hover:text-blue-300 transition-colors facebook">
               <Facebook size={24} />
             </a>
-            <a href="#" className="text-pink-400 hover:text-pink-300 transition-colors insta">
+            <a href="https://www.instagram.com/sujal_pokhrel999/" className="text-pink-400 hover:text-pink-300 transition-colors insta">
               <Instagram size={24} />
             </a>
-            <a href="#" className="text-white hover:text-gray-300 transition-colors git">
+            <a href="https://github.com/sujalpokhrel999" className="text-white hover:text-gray-300 transition-colors git">
               <Github size={24} />
             </a>
-            <a href="#" className="text-blue-500 hover:text-blue-400 transition-colors linkedin">
+            <a href="https://www.linkedin.com/in/sujal-pokhrel-8431761b2/" className="text-blue-500 hover:text-blue-400 transition-colors linkedin">
               <Linkedin size={24} />
             </a>
           </div>
